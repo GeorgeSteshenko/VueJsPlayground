29b019f8575f19ef63157ec102942f25b3312fd0
Yegor Steshenko <yegorsteshenko@Yegors-MacBook-Pro.local>
2017-02-10 18:06:26 +0200

    Weather app updates

--- src/components/weather.vue
+++ src/components/weather.vue
@@ -1,15 +1,11 @@
 <template>
     <div class="weather">
-        <h1>{{ title }}</h1>
-        <!-- <input type="text" v-model="forecast.city"> -->
+        <h1>{{ title }} for {{ forecast.name }}, {{ forecast.country }}</h1>
         <div class="weather">
-            <!-- <p>{{ forecast.city }}, {{ forecast.country }}</p>
-            <p>{{ formatDate(forecast.date) }}</p>
-            <p>{{ forecast.weather }}</p>
-            <p>{{ Math.round(forecast.temperature) }} &#0176;C</p> -->
-            <input type="text" v-model="forecast.name">
-            <p>{{ forecast.name }}</p>
-            <p>{{ forecast.dt }}</p>
+            <input type="text" v-model="forecast.name" @input="weather">
+            <h2>{{ forecast.name }}, {{ forecast.country }}</h2>
+            <h4>{{ Math.round(forecast.temperature) }} &#0176;C</h4>
+            <p>{{ formatDate(forecast.dt) }}</p>
             <p>{{ forecast.sys }}</p>
         </div>
     </div>
@@ -22,13 +18,12 @@
         data () {
             return {
                 title: 'Weather Forecast',
-                forecast: [
-                    // {
-                    //     city: '',
-                    //     weather: '',
-                    //     temperature: ''
-                    // }
-                ]
+                forecast: {
+                    name: 'London',
+                    dt: '',
+                    country: '',
+                    temperature: ''
+                }
             }
         },
 
@@ -37,24 +32,22 @@
         },
 
         methods: {
-            weather: function() {
+            weather: _.debounce(
+                function() {
+                    this.$http.get('http://api.openweathermap.org/data/2.5/weather?q='+this.forecast.name+'&units=metric&APPID=d55314939b523c73346bafda8fbaadca')
+                        .then(function(response) {
+                            this.forecast = response.data;
+                            this.forecast.country = response.data.sys.country;
+                            this.forecast.temperature = response.data.main.temp;
+                        });
+                },
+            1000),
 
-                this.$http.get('http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=d55314939b523c73346bafda8fbaadca')
-                    .then(function(response) {
-                        this.forecast = response.data;
-                        // this.forecast.city = response.data.name;
-                        // this.forecast.country = response.data.sys.country;
-                        // this.forecast.date = response.data.dt;
-                        // this.forecast.weather = response.data.weather;
-                        // this.forecast.temperature = response.data.main.temp;
-                    });
-            },
-
-            // formatDate: function(date) {
-            //     let theDate = new Date(this.forecast.date * 1000)
-            //     let dateString = theDate.toGMTString()
-            //     return dateString
-            // }
+            formatDate: function(date) {
+                let theDate = new Date(this.forecast.dt * 1000)
+                let dateString = theDate.toGMTString()
+                return dateString
+            }
         }
     }
 </script>

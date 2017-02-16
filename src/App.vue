6120f10a0b3a75726bd19877a51b35fbbbac377d
Yegor Steshenko <yegorsteshenko@Yegors-MacBook-Pro.local>
2017-02-16 18:21:03 +0200

    weather app updates

--- src/App.vue
+++ src/App.vue
@@ -33,10 +33,10 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  /*color: #2c3e50;*/
   margin: 60px;
 }
 .menu {
--- src/components/weather.vue
+++ src/components/weather.vue
@@ -1,12 +1,34 @@
 <template>
-    <div class="weather">
-        <h1>{{ title }} for {{ forecast.name }}, {{ forecast.country }}</h1>
-        <div class="weather">
-            <input type="text" v-model="forecast.name" @input="weather">
-            <h2>{{ forecast.name }}, {{ forecast.country }}</h2>
-            <h4>{{ Math.round(forecast.temperature) }} &#0176;C</h4>
-            <p>{{ formatDate(forecast.dt) }}</p>
-            <p>{{ forecast.sys }}</p>
+    <div class="container">
+        <div class="jumbotron">
+            <h1>{{ title }} for {{ forecast.name }}, {{ forecast.country }}</h1>
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search for..." v-model.lazy="forecast.name">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button" @click="weather">Show</button>
+                        </span>
+                    </div>
+                    <hr>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">{{ forecast.name }}, {{ forecast.country }}</h3>
+                        </div>
+                        <div class="panel-body">
+                            <p>
+                                {{ Math.round(forecast.temperature) }} &#0176;C
+                                <span :class="'icon-'+forecast.weather.iconId"></span>
+                            </p>
+                            <p>{{ formatDate(forecast.dt) }}</p>
+                            <p>{{ forecast.weather.main }}</p>
+                            <p>{{ forecast.weather.description }}</p>
+                            <p>{{ forecast.weather.iconId }}</p>
+                            <p>{{ forecast.sys }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -22,7 +44,14 @@
                     name: 'London',
                     dt: '',
                     country: '',
-                    temperature: ''
+                    temperature: '',
+                    weather: [
+                        {
+                            main: '',
+                            description: '',
+                            iconId: ''
+                        }
+                    ]
                 }
             }
         },
@@ -32,16 +61,18 @@
         },
 
         methods: {
-            weather: _.debounce(
+            weather:
                 function() {
                     this.$http.get('http://api.openweathermap.org/data/2.5/weather?q='+this.forecast.name+'&units=metric&APPID=d55314939b523c73346bafda8fbaadca')
                         .then(function(response) {
                             this.forecast = response.data;
                             this.forecast.country = response.data.sys.country;
                             this.forecast.temperature = response.data.main.temp;
+                            this.forecast.weather.main = response.data.weather[0].main;
+                            this.forecast.weather.description = response.data.weather[0].description;
+                            this.forecast.weather.iconId = response.data.weather[0].icon;
                         });
                 },
-            1000),
 
             formatDate: function(date) {
                 let theDate = new Date(this.forecast.dt * 1000)
@@ -53,5 +84,7 @@
 </script>
 
 <style scoped>
-
+hr {
+    border-color: #ccc;
+}
 </style>

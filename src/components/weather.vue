f64623ea8ec797c389a9fb990a021a63977fb40f
Yegor Steshenko <yegorsteshenko@Yegors-MacBook-Pro.local>
2017-02-19 22:27:46 +0200

    Weather app updates with user location

--- src/components/weather.vue
+++ src/components/weather.vue
@@ -2,11 +2,12 @@
     <div class="container">
         <div class="jumbotron">
             <h1>{{ title }} for {{ forecast.name }}, {{ forecast.country }}</h1>
+
             <div class="row">
                 <div class="col-lg-6">
                     <div class="input-group">
                         <form class="form-inline" @submit.prevent="weather">
-                        <input type="text" class="form-control" placeholder="Search for..." v-model.lazy="forecast.name">
+                        <input type="text" class="form-control" placeholder="Search for..." v-model.lazy="userLocation.city">
                             <span class="input-group-btn">
                                 <button class="btn btn-default" type="button" @click="weather">Get Weather</button>
                             </span>
@@ -25,9 +26,11 @@
                             <li class="list-group-item lead text-capitalize"><b>date:</b> {{ formatDate(forecast.dt) }}</li>
                             <li class="list-group-item lead text-capitalize"><b>description:</b> {{ forecast.weather.description }}</li>
                             <li class="list-group-item lead"><b>Pressure:</b> <i class="wi wi-barometer"></i> {{ forecast.pressure }} hPa</li>
-                            <li class="list-group-item lead"><b>Humidity:</b> <i class="wi wi-humidity"></i> {{ forecast.humidity }}%</li>
-                            <li class="list-group-item lead"><b>Wind:</b> <i class="wi wi-strong-wind"></i> {{ forecast.wind }} meter/sec, ({{ toKmHr(forecast.wind) }} km/Hr <i class="wi wi-train
-"></i>)</li>
+                            <li class="list-group-item lead"><b>Humidity:</b> <i class="wi wi-humidity"></i> {{ forecast.humidity }}</li>
+                            <li class="list-group-item lead">
+                                <b>Wind:</b> <i class="wi wi-strong-wind"></i> {{ forecast.wind }} meter/sec,
+                                it's {{ toKmHr(forecast.wind) }} km/Hr <i class="wi wi-train"></i>
+                            </li>
                             <li class="list-group-item lead"><b>Sunrise:</b> <i class="wi wi-sunrise"></i> {{ formatTime(forecast.sunrise) }}</li>
                             <li class="list-group-item lead"><b>Sunset:</b> <i class="wi wi-sunset"></i> {{ formatTime(forecast.sunset) }}</li>
                         </ul>
@@ -45,8 +48,11 @@
         data () {
             return {
                 title: 'Weather Forecast',
+                userLocation: {
+                    city: '',
+                    countryCode: ''
+                },
                 forecast: {
-                    name: 'London',
                     dt: '',
                     country: '',
                     temperature: '',
@@ -67,13 +73,31 @@
         },
 
         created: function(){
-          this.weather();
+            this.getUserLocation();
+            this.weather();
         },
 
         methods: {
-            weather:
+            getUserLocation:
                 function() {
-                    this.$http.get('http://api.openweathermap.org/data/2.5/weather?q='+this.forecast.name+'&units=metric&APPID=d55314939b523c73346bafda8fbaadca')
+                    this.$http.get('http://ip-api.com/json')
+                        .then(function(response) {
+                            this.userLocation.city = response.body.city;
+                            this.userLocation.countryCode = response.body.countryCode;
+                            this.weather();
+                        })
+                        .catch(function(err) {
+                            console.log(err);
+                        });
+                },
+            weather: _.debounce(
+                function() {
+                    let apiWeather = 'http://api.openweathermap.org/data/2.5/weather?q='
+                    let location = this.userLocation.city + ','
+                    let countryCode = this.userLocation.countryCode
+                    let units = '&units=metric'
+                    let apiKey = '&APPID=d55314939b523c73346bafda8fbaadca'
+                    this.$http.get(apiWeather + location + countryCode + units + apiKey)
                         .then(function(response) {
                             this.forecast = response.data;
                             this.forecast.country = response.data.sys.country;
@@ -84,11 +108,11 @@
                             this.forecast.weather.main = response.data.weather[0].main;
                             this.forecast.weather.description = response.data.weather[0].description;
                             this.forecast.weather.iconId = response.data.weather[0].icon;
-                            this.forecast.weather.iconId = response.data.weather[0].icon;
                             this.forecast.sunrise = response.data.sys.sunrise;
                             this.forecast.sunset = response.data.sys.sunset;
                         });
-                },
+                }, 200
+            ),
 
             formatDate: function(date) {
                 let theDate = new Date(date * 1000).toLocaleString()
